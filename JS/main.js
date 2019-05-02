492f20a1509a31b40c8bc724194a863238a90006
amarjt <mjamarmj@gmail.com>
2019-05-02 20:41:21 +0530

    V 0.0.2
    final ver ~ without form validation

--- JS/main.js
+++ JS/main.js
@@ -3,34 +3,45 @@ $(document).ready(function () {
         e.preventDefault();
         $("#signIn").css("left", "50vw");
         $("#signUp").css({
-            "opacity": "1",
-            "z-index": "4",
+            "z-index": "1",
             "left": "50vw"
         });
         $("#signUpOverlay").css({
-            "z-index": "1",
-            "opacity": ".1"
+            "z-index": "0",
+            "left": "0vw",
         });
-        // $("#signUpOverlay .overlay-content").css("opacity", "0");
-        // $("#signUpOverlay .overlay-content").css("margin-left", "20rem");
-        $(".row").css("left", "0vw");
-
+        $("#signUpOverlay .overlay-content").css({
+            "margin-left": "70rem",
+        });
+        $("#signInOverlay").css({
+            "z-index": "1",
+            "left": "0vw",
+        });
+        $("#signInOverlay .overlay-content").css({
+            "margin-left": "0rem",
+        });
     });
+
     $("#signIpOverlayBtn").click(function (e) {
         e.preventDefault();
         $("#signIn").css("left", "0vw");
         $("#signUp").css({
-            "opacity": ".5",
             "z-index": "0",
             "left": "0vw",
         });
         $("#signUpOverlay").css({
-            "z-index": "3",
-            "opacity": "1",
+            "z-index": "1",
+            "left": "50vw",
         });
-        $(".row").css("left", "50vw");
-        // $("#signInOverlay .overlay-content").css("opacity", "0");
-        // $("#signInOverlay .overlay-content").css("margin-right", "20rem");
-
+        $("#signUpOverlay .overlay-content").css({
+            "margin-left": "0rem",
+        });
+        $("#signInOverlay").css({
+            "z-index": "0",
+            "left": "50vw",
+        });
+        $("#signInOverlay .overlay-content").css({
+            "margin-left": "-70rem",
+        });
     });
 });
